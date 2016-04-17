7af48a6798af48c0c27346fbd1feb56c6673cc4c
weswhitney <wesleywhitney@gmail.com>
2016-04-17 17:27:24 -0600

    adding delete to tutorial

--- components/todos.jsx
+++ components/todos.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import NewTodo from './NewTodo'
-import { addTodo } from '../actions'
+import { addTodo, deleteTodo } from '../actions'
 
 const Todos = ({todos, dispatch}) => (
   <div>
     <h1>Todos</h1>
-	<NewTodo onChange={e => {
-		if(e.keyCode == 13) {
-			dispatch(addTodo(e.target.value))
-			e.target.value = ''
-		}
-	}}/>
-    {todos.map(todo => <p key={todo}>{todo}</p>)}
+    <NewTodo onChange={e => {
+      if(e.keyCode == 13){
+        dispatch(addTodo(e.target.value))
+        e.target.value = ''
+      }
+    }}/>
+    {todos.map((todo, index) => <p key={index}>{todo} <button onClick={e => {
+      dispatch(deleteTodo(index))
+    }}>X</button></p>)}
   </div>
 )
 
@@ -24,4 +26,3 @@ function mapStateToProps(todos) {
 }
 
 export default connect(mapStateToProps)(Todos)
-
--- reducers/todos.js
+++ reducers/todos.js
@@ -3,7 +3,9 @@ import Immutable from 'immutable'
 export default (state = Immutable.List(['Code More!']), action) => {
   switch(action.type) {
     case 'addTodo':
-      return state.push(action.todo)
+      return state.unshift(action.todo)
+    case 'deleteTodo':
+        return state.filter((todo, index) => index !== action.index)
     default:
       return state
   }
